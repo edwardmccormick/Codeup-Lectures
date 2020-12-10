ca1558815492555171b0789e8cb68be799c64395
edwardmccormick <ted.mccormick@gmail.com>
2020-12-09 22:16:32 -0600

    Tightened up some code and added Advent of Code

---
+++ js/DOMevents.js
@@ -0,0 +1,17 @@
+console.log("I know it's hard to believe but here I am!")
+
+console.log(document.getElementById("head"))
+
+
+document.getElementById("head").addEventListener('mouseover', function() {console.log("head")})
+
+
+
+document.getElementById("shoulder").addEventListener('mouseover', function() {console.log("shoulders")})
+
+
+document.getElementById("knees").addEventListener('mouseover', function() {console.log("knees")})
+
+document.getElementById('and').addEventListener("click", function() {console.log("and")})
+
+
